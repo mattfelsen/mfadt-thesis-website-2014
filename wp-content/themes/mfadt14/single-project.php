8e9c6de3a757767fb9d1d7492e2e9c4e92df994d
Matt Felsen <mattfelsen@gmail.com>
2014-03-28 14:44:03 -0400

    Began hooking up templates to database and renamed detail pages to proper WP conventions (eg single-project.php)

---
+++ wp-content/themes/mfadt14/single-project.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Project Detail
+*/
+
+get_header();
+
+?>
+
+<!-- html goes here -->
+<div class="sixteen columns mainContainer">
+	<h1><?php the_title(); ?></h1>
+
+	<div class="four columns">
+		<h4>Project description and stuff. Hook me up to the database!</a></h4>
+	</div>
+
+</div>
+
+<?php get_footer(); ?>
