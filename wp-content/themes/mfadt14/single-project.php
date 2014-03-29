3ee79be2f6d63e52a1ac7ca85411115baf85c70e
Matt Felsen <mattfelsen@gmail.com>
2014-03-29 17:16:03 -0400

    Continuing work hooking up templates to database

--- wp-content/themes/mfadt14/single-project.php
+++ wp-content/themes/mfadt14/single-project.php
@@ -10,9 +10,22 @@ get_header();
 <!-- html goes here -->
 <div class="sixteen columns mainContainer">
 	<h1><?php the_title(); ?></h1>
+	<h3><?= types_render_field('short-description') ?></h3>
 
-	<div class="four columns">
-		<h4>Project description and stuff. Hook me up to the database!</a></h4>
+	<div class="six columns">
+		<p><?= types_render_field('long-description') ?></p>
+		<p><?= types_render_field('project-website') ?></p>
+		<p>
+			<h5>Thesis Faculty</h5>
+			Fall: <?= types_render_field('thesis-faculty-fall') ?><br>
+			Spring: <?= types_render_field('thesis-faculty-spring') ?><br>
+			<h5>Writing & Research Faculty</h5>
+			Fall: <?= types_render_field('writing-faculty-fall') ?><br>
+			Spring: <?= types_render_field('writing-faculty-spring') ?><br>
+		</p>
+	</div>
+	<div class="eight columns">
+		<?= types_render_field('featured-image') ?>
 	</div>
 
 </div>
--- wp-content/themes/mfadt14/template-project-list.php
+++ wp-content/themes/mfadt14/template-project-list.php
@@ -5,8 +5,19 @@ Template Name: Project List
 
 get_header();
 
-$args = array ('post_type' => 'project', 'posts_per_page' => '-1', 'orderby' => 'rand');
-$query = new WP_Query( $args );
+$query = "SELECT p.post_title AS title, p.post_name AS slug, s.post_title AS student FROM wp_posts p, wp_posts s, wp_postmeta m WHERE m.meta_key = 'wpcf-student-name' AND s.post_name = m.meta_value AND p.post_type = 'project' AND p.post_status = 'publish';";
+$result = $wpdb->get_results($query);
+
+// Old native WP_Query call for posts
+// Deprecated in favor of the raw call above
+// Might want to bring this back at some point so that functions like the_permalink() work again
+// $args = array (
+// 	'post_type' => 'project',
+// 	'posts_per_page' => '-1',
+// 	'orderby' => 'rand',
+// 	'meta_key' => 'wpcf-student-name'
+// );
+// $query = new WP_Query( $args );
 
 ?>
 
@@ -14,14 +25,19 @@ $query = new WP_Query( $args );
 <div class="sixteen columns mainContainer">
 	<h1>Projects</h1>
 	
-	<?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+	<?php //if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+	<?php foreach ($result as $project) { ?>
 	
 	<div class="four columns">
-		<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
-		
+		<!-- <h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4> -->
+		<h4><a href="projects/<?= $project->slug  ?>"><?= $project->title ?></a></h4>
+		<?= $project->student ?>
 	</div>
 	
-	<?php endwhile; endif; ?>
+
+
+	<?php } ?>
+	<?php //endwhile; endif; ?>
 </div>
 
 <?php get_footer(); ?>
