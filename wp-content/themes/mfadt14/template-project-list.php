10c5f25631634730840c70d51dc1b56fc3f89ac0
Matt Felsen <mattfelsen@gmail.com>
2014-04-18 22:07:32 -0400

    Updates to faculty, footer, search results, project detail, and project page
    faculty
    - added faculty to database
    - print title & content on faculty page
    
    footer
    - list faculty from db
    - added committees & members (hard-coded)
    
    search results
    - worked on cleaning up formatting for results
    - only students & projects are displayed (no faculty)
    - results are displayed consistently with title first and students underneath, regardless of whether the search matched a student or project
    - added project thumbnail, category & tags for each project, inside an html comment
    
    project detail
    - only print faculty once if the same between fall & spring semesters
    - otherwise print both with Fall/Spring label
    - added category & tags, inside html comments
    
    project list
    - added category & tags for each project, inside html comments

--- wp-content/themes/mfadt14/template-project-list.php
+++ wp-content/themes/mfadt14/template-project-list.php
@@ -14,7 +14,7 @@ $query = new WP_Query( $args );
 	<h1>Projects</h1>
 <div class="sixteen columns mainContainer">
 
-	
+
 	<?php
 
 	if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
@@ -23,21 +23,23 @@ $query = new WP_Query( $args );
 		$students = types_child_posts('student');
 		foreach ($students as $student) { $students_list[] = $student->post_title; }
 		$students = join(" + ", $students_list);
-		
+
 	?>
-	
+
 	<div class="masonry columns">
 		<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
 		<p><?= $students ?></p>
-		
-	    <? if (has_post_thumbnail()) : ?>
-		    <img class="projectThumb" src="<? $src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large'); print $src[0]; ?>">
-			<? else: ?>
-			<img class="projectThumb" src="assets/img/no-thumbnail-<? $r=rand(0,3);if($r>2)$o='sm';elseif($r>1)$o='md';else $o='lg'; print $o; ?>.jpg">
+		<!-- <? the_category(); ?> -->
+		<!-- <?= join(', ', get_the_tags()); ?> -->
+
+	  <? if (has_post_thumbnail()) : ?>
+		<img class="projectThumb" src="<? $src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large'); print $src[0]; ?>">
+		<? else: ?>
+		<img class="projectThumb" src="assets/img/no-thumbnail-<? $r=rand(0,3);if($r>2)$o='sm';elseif($r>1)$o='md';else $o='lg'; print $o; ?>.jpg">
 		<?php endif; ?>
-		
+
 	</div>
-	
+
 	<?php endwhile; endif; ?>
 </div>
 
--- wp-content/themes/mfadt14/template-student-list.php
+++ wp-content/themes/mfadt14/template-student-list.php
@@ -19,12 +19,12 @@ $query = new WP_Query( $args );
 	if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
 		$project = $wpdb->get_row("SELECT post_title, post_name FROM wp_posts WHERE id = '".wpcf_pr_post_get_belongs(get_the_ID(), 'project')."'");
 	?>
-	
+
 	<div class="four columns">
 		<h4><a href="project/<?= $project->post_name ?>"><?php the_title(); ?></a></h4>
 		<p><?= $project->post_title ?></p>
 	</div>
-	
+
 	<?php endwhile; endif; ?>
 
 </div>
