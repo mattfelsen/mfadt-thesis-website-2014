37c3643985c36f1f600dc0593bce70d32e46d7bb
Matt Felsen <mattfelsen@gmail.com>
2014-04-29 10:03:15 -0400

    Stripped all WP from teaser site, added google analytics & twitter/fb meta tags

--- wp-content/themes/mfadt14/single-student.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$project = $wpdb->get_var("SELECT post_name FROM wp_posts WHERE id = '".wpcf_pr_post_get_belongs(get_the_ID(), 'project')."'");
-
-wp_redirect(home_url() . "/project/$project");
-exit;
-
-?>
