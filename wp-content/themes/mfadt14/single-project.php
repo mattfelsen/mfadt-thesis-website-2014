0ea356c2ae8b79ebbca82d61503218ab3cd41a85
Fei <fei.fei2liu@gmail.com>
2014-05-01 00:01:09 -0400

    Responsive styling for single pages, tidied up footer and other small changes

--- wp-content/themes/mfadt14/single-project.php
+++ wp-content/themes/mfadt14/single-project.php
@@ -15,18 +15,6 @@ $thesis_spring = types_render_field('thesis-faculty-spring');
 $writing_fall = types_render_field('writing-faculty-fall');
 $writing_spring = types_render_field('writing-faculty-spring');
 
-// Set up category name
-$category = get_the_category();
-$category = $category[0]->cat_name;
-
-// Set up tags
-$a = array();
-$tags = get_the_tags();
-foreach($tags as $t) {
-	$a[] = $t->name;
-}
-$tags = join(', ', $a);
-
 ?>
 
 <!-- html goes here -->
@@ -42,9 +30,12 @@ $tags = join(', ', $a);
 		</div>
 		<div class="student-info-social">
 			<div class="studentContactInfo">
-				<a href="<?= $student->fields['personal-website'] ?>" target="_blank"><div class="personalWebsiteLink"></div></a>
-				<a href="<?= $student->fields['linkedin'] ?>" target="_blank"><div class="linkedInLink"></div></a>
-				<a href="https://twitter.com/<?= $student->fields['twitter'] ?>" target="_blank"><div class="twitterLink"></div></a>			
+				<h5>Learn more:</h5>
+				<div class="social-icons">
+					<a href="<?= $student->fields['personal-website'] ?>" target="_blank"><i class="fa fa-desktop"></i></a>
+					<a href="https://twitter.com/<?= $student->fields['twitter'] ?>" target="_blank"><i class="fa fa-twitter"></i></a>
+					<a href="<?= $student->fields['linkedin'] ?>" target="_blank"><i class="fa fa-linkedin"></i></a>
+				</div>
 			</div>
 		</div>
 		<div class="student-info-image">
@@ -55,40 +46,46 @@ $tags = join(', ', $a);
 	</section>
 	<?php } ?>
 
-	<hr>
 
 	<section class="project">
-		<h2><?php the_title(); ?></h2>
-		<div class="projectInfoHolder">
-			<div class="projectTextHolder">
-				<h3><?= types_render_field('short-description') ?></h3>
-				<!-- <?= $category ?> -->
-				<!-- <?= $tags ?> -->
-				<p ><?= types_render_field('long-description') ?></p>
-				<!-- follow the same format of "Project Website" rather than displaying URL target="_blank" -->
-				<p><a href="<?= types_render_field('project-website', array('output' => 'raw')) ?>" target="_blank">View Project Website</a></p>
-				<p>
-					<h5>Thesis Faculty</h5>
-	
-					<?php if ($thesis_fall == $thesis_spring) : ?>
-					<p class="facultyNames"><?= $thesis_fall ?></p><br>
-					<?php else: ?>
-					<p class="facultyNames">Fall: <?= $thesis_fall ?></p>
-					<p class="facultyNames">Spring: <?= $thesis_spring ?></p><br>
-					<?php endif; ?>
-	
-					<h5>Writing & Research Faculty</h5>
-					<?php if ($thesis_fall == $thesis_spring) : ?>
-					<p class="facultyNames"><?= $writing_fall ?></p><br>
-					<?php else: ?>
-					<p class="facultyNames">Fall: <?= $writing_fall ?></p>
-					<p class="facultyNames">Spring: <?= $writing_spring ?></p><br>
-					<?php endif; ?>
-				</p>
+		<div class="project-info-text">
+			<h3><?php the_title(); ?></h3>
+			<h4><?= types_render_field('short-description') ?></h4>
+			
+			<p ><?= types_render_field('long-description') ?></p>
+			<!-- follow the same format of "Project Website" rather than displaying URL target="_blank" -->
+			<p><a href="<?= types_render_field('project-website', array('output' => 'raw')) ?>" target="_blank">View Project Website</a></p>
+			
+			<div class="project-meta">
+				<p class="category">Filed under: <?php the_category(','); ?> </p> 
+				<p class="tag"><?php the_tags('Tagged with: '); ?></p>
 			</div>
-			<div class="projectImageHolder">
+
+			<h5>Thesis Faculty</h5>
+		
+			<?php if ($thesis_fall == $thesis_spring) : ?>
+				<p class="facultyNames"><?= $thesis_fall ?></p><br>
+			<?php else: ?>
+				<p class="facultyNames">Fall: <?= $thesis_fall ?></p>
+				<p class="facultyNames">Spring: <?= $thesis_spring ?></p><br>
+			<?php endif; ?>
+		
+			<h5>Writing & Research Faculty</h5>
+			<?php if ($thesis_fall == $thesis_spring) : ?>
+				<p class="facultyNames"><?= $writing_fall ?></p><br>
+			<?php else: ?>
+				<p class="facultyNames">Fall: <?= $writing_fall ?></p>
+				<p class="facultyNames">Spring: <?= $writing_spring ?></p><br>
+			<?php endif; ?>
+				</p>
+				
+		</div>
+
+		
+		<div class="project-info-image">
 			<center><?= types_render_field('media', array('width' => 600)) ?></center>
 			</div>
+		<div class="clear"></div>
 		</div>	
 	</section>
 	
