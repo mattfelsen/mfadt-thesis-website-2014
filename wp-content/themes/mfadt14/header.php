e6c99dd4138ad16b8998c2075c8f8569e4922702
Matt Felsen <mattfelsen@gmail.com>
2014-05-09 16:30:32 -0400

    Updated FB open graph meta tags

--- wp-content/themes/mfadt14/header.php
+++ wp-content/themes/mfadt14/header.php
@@ -24,11 +24,11 @@
     <meta name="twitter:image:src" content="http://mfadt.parsons.edu/2014/assets/img/hero/embed.jpg">
     <meta name="twitter:domain" content="mfadt.parsons.edu">
 
-    <meta property="og:site_name" content="MFAD+T Thesis Show 2014">
-    <meta property="og:title" content="Join us this May for a week-long exhibition of 70+ projects by emerging artists, designers, coders, makers, hackers, educators, and thinkers.">
+<meta property="og:site_name" content="MFAD+T Thesis Show 2014">
+    <meta property="og:title" content="MFAD+T Thesis Show 2014">
     <meta property="og:url" content="http://mfadt.parsons.edu/2014/">
-    <meta property="og:image" content="http://mfadt.parsons.edu/2014/assets/img/hero/embed.jpg">
-    <meta property="og:description" content="Symposium: May 16-17. Exhibition: May 17-25. Reception: May 21.">
+    <meta property="og:image" content="http://mfadt.parsons.edu/2014/assets/img/hero/embed.v2.jpg">
+    <meta property="og:description" content="Join us this May for a week-long exhibition of 70+ projects by emerging artists, designers, coders, makers, hackers, educators, and thinkers. Symposium: May 16-17. Exhibition: May 17-25. Reception: May 21.">
 
 	<?php wp_head(); ?></head>
 <body <?php body_class(); ?>>
