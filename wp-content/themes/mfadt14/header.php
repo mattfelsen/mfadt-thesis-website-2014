27a4749d934384f442f73b5294d80ac5b6691eac
Matt Felsen <mattfelsen@gmail.com>
2014-05-09 03:16:41 -0400

    Added social meta tags from teaser site

--- wp-content/themes/mfadt14/header.php
+++ wp-content/themes/mfadt14/header.php
@@ -15,6 +15,21 @@
 	<link href='http://fonts.googleapis.com/css?family=Exo+2:400,100,100italic,200,200italic,300,300italic,400italic,500,500italic,600,600italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,100italic,300italic,400italic,700italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+
+	<meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@mfadt">
+    <meta name="twitter:title" content="MFAD+T Thesis Show 2014">
+    <meta name="twitter:description" content="Join us this May for a week-long exhibition of 70+ projects by emerging artists, designers, coders, makers, hackers, educators, and thinkers.">
+    <meta name="twitter:creator" content="@mfadt">
+    <meta name="twitter:image:src" content="http://mfadt.parsons.edu/2014/assets/img/hero/embed.jpg">
+    <meta name="twitter:domain" content="mfadt.parsons.edu">
+
+    <meta property="og:site_name" content="MFAD+T Thesis Show 2014">
+    <meta property="og:title" content="Join us this May for a week-long exhibition of 70+ projects by emerging artists, designers, coders, makers, hackers, educators, and thinkers.">
+    <meta property="og:url" content="http://mfadt.parsons.edu">
+    <meta property="og:image" content="http://mfadt.parsons.edu/2014/assets/img/hero/embed.jpg">
+    <meta property="og:description" content="Symposium: May 16-17. Exhibition: May 17-25. Reception: May 21.">
+
 	<?php wp_head(); ?></head>
 <body <?php body_class(); ?>>
 	<nav>
